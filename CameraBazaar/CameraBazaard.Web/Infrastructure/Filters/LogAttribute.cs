84991eb183c984af223854e4620206def54ea7fb
killemil <esend3@yahoo.com>
2017-11-18 17:18:33 +0200

    LogFilter

---
+++ CameraBazaar/CameraBazaard.Web/Infrastructure/Filters/LogAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace CameraBazaar.Web.Infrastructure.Filters
+{
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using System;
+    using System.IO;
+
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            using (var writer = new StreamWriter("log.txt", true))
+            {
+                var date = DateTime.UtcNow;
+                var ipAdress = context.HttpContext.Connection.RemoteIpAddress;
+                var username = context.HttpContext.User.Identity.Name ?? "Anonymous";
+                var controller = context.Controller.GetType().Name;
+                var action = context.RouteData.Values["action"];
+
+                var logMessage = $"{date} - {ipAdress} - {username} - {controller}.{action}";
+
+                if (context.Exception != null)
+                {
+                    var exceptionType = context.Exception.GetType().Name;
+                    var exceptiomMessage = context.Exception.Message;
+
+                    logMessage = $"[!] {logMessage} - {exceptionType} - {exceptiomMessage}";
+                }
+
+                writer.WriteLine(logMessage);
+            }
+        }
+    }
+}
---
+++ CameraBazaar/CameraBazaard.Web/Infrastructure/Filters/MeassureTimeAttribute.cs
@@ -0,0 +1,39 @@
+﻿namespace CameraBazaar.Web.Infrastructure.Filters
+{
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    public class MeassureTimeAttribute : ActionFilterAttribute
+    {
+        private Stopwatch stopwatch;
+
+        public MeassureTimeAttribute()
+        {
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            this.stopwatch = Stopwatch.StartNew();
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            this.stopwatch.Stop();
+
+            using (var writer = new StreamWriter("action-times.txt", true))
+            {
+                var date = DateTime.UtcNow;
+                var controller = context.Controller.GetType().Name;
+                var action = context.RouteData.Values["action"];
+                var elapsedTime = this.stopwatch.Elapsed;
+
+                var log = $"{date} - {controller}.{action} - {elapsedTime}";
+
+                writer.WriteLine(log);
+            }
+        }
+
+    }
+}
